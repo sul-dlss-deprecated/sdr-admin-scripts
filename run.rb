ecbc1919ac8014669b29ea862cc1c71df8c1a9c5
Tony Zanella <tony.zanella@gmail.com>
2019-03-18 16:23:08 -0700

    Add scratch space

---
+++ run.rb
@@ -0,0 +1,13 @@
+require './lib/workflow_status_updater.rb'
+
+# This is a scratch space to build scripts, for example:
+# druids = ['ab123cd4567']
+#
+# druids.each do |druid|
+#  updater = WorkflowStatusUpdater.new(druid: druid,
+#                                      workflow: 'accessionWF',
+#                                      step: 'descriptive-metadata',
+#                                      status: 'waiting')
+#  puts updater.inspect
+# end
+
